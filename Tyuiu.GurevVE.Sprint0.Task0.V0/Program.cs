b46bcecb33f2b09419f19c1c88696e60f94fab0c
vladgurev <mskacov@gmail.com>
2026-09-11 01:00:45 +0700

    Initial commit

---
+++ Tyuiu.GurevVE.Sprint0.Task0.V0/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Tyuiu.GurevVE.Sprint0.Task0.V0;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Tyuiu.GurevVE.Sprint0/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Tyuiu.GurevVE.Sprint0;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
