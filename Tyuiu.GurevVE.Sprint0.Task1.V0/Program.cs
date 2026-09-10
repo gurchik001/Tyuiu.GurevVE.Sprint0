af20aa0792375448e32be9d367887954c5536cb3
vladgurev <mskacov@gmail.com>
2026-09-11 01:17:02 +0700

    Task1

---
+++ Tyuiu.GurevVE.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Tyuiu.GurevVE.Sprint0.Task1.V0;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string name = "Влад";
+        int age = 18;
+        Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет.");
+        Console.ReadKey();
+    }
+}
